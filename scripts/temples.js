eb76d148761ea55804310c3b6fc665560c810722
AeuroShaw <feyuro@gmail.com>
2026-03-14 16:25:34 +0100

    Update temples.js

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,6 +1,18 @@
-const year = document.querySelector("#year");
-const lastModified = document.querySelector("#lastModified");
+const year=document.querySelector("#year");
+const lastModified=document.querySelector("#lastModified");
+const menuButton=document.querySelector("#menu");
+const navigation=document.querySelector("nav");
 
-year.textContent = new Date().getFullYear();
+year.textContent=new Date().getFullYear();
 
-lastModified.textContent = "Last Modified: " + document.lastModified;
+lastModified.textContent="Last Modified: "+document.lastModified;
+
+menuButton.addEventListener("click",()=>{
+navigation.classList.toggle("open");
+
+if(menuButton.textContent==="☰"){
+menuButton.textContent="✖";
+}else{
+menuButton.textContent="☰";
+}
+});
